1d59ab51123d6687bdc1c3b62d5ac8c4b5173833
e <357589728@qq.com>
2017-10-27 19:23:31 +0800

    jquery

---
+++ ajax/jquery的ajax的方法/14.php
@@ -0,0 +1,10 @@
+<?php 
+    // $uname = $_GET['username'];
+    // $pw = $_GET['password'];
+
+    $uname = $_POST['username'];
+    $pw = $_POST['password'];
+
+    // echo $uname.'-----'.$pw;
+    echo '{"username":"'.$uname.'","password":"'.$pw.'"}';
+ ?>
